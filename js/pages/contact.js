af087a2b52be94c99b0b96b69d47bf03f3d0e3bc
16LouisM <louis.mash02@gmail.com>
2026-06-25 06:12:25 +0200

    Implement contact form submission; add message handling and reset functionality

--- js/pages/contact.js
+++ js/pages/contact.js
@@ -2,7 +2,10 @@ import { db } from "../firebase-config.js";
 
 import {
     doc,
-    getDoc
+    getDoc,
+    collection,
+    addDoc,
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 /* =========================
@@ -35,4 +38,64 @@ export async function initContact() {
 
     document.getElementById("github-card").href =
         data.github || "#";
+
+    const form = document.getElementById("contactForm");
+
+    if (form && !form.dataset.initialized) {
+
+        form.addEventListener("submit", sendMessage);
+
+        form.dataset.initialized = "true";
+    }
+}
+async function sendMessage(event) {
+
+    event.preventDefault();
+
+    const submitBtn =
+        document.querySelector(".contact-form button");
+
+    const name =
+        document.getElementById("name").value.trim();
+
+    const email =
+        document.getElementById("email").value.trim();
+
+    const message =
+        document.getElementById("message").value.trim();
+
+    if (!name || !email || !message) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    try {
+
+        submitBtn.disabled = true;
+        submitBtn.textContent = "Sending...";
+
+        await addDoc(collection(db, "messages"), {
+            name,
+            email,
+            message,
+            status: "unread",
+            createdAt: serverTimestamp()
+        });
+
+        alert("Message sent successfully!");
+
+        document.getElementById("contactForm").reset();
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Failed to send message.");
+
+    } finally {
+
+        submitBtn.disabled = false;
+        submitBtn.textContent = "Send Message";
+
+    }
 }
